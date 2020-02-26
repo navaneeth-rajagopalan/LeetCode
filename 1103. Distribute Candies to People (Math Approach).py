2022edc3d6a7698ed1c5947c4a4e662fc001ac04
Navaneeth Rajagopalan <navaneethnvnthr@gmail.com>
2020-02-26 17:25:01 +1100

    Update math approach for 1103
    Signed-off-by: Navaneeth Rajagopalan <navaneethnvnthr@gmail.com>

--- 1103. Distribute Candies to People (Math Approach).py
+++ 1103. Distribute Candies to People (Math Approach).py
@@ -8,6 +8,7 @@ class Solution:
         solution = [0] * num_people
         
         for i in range(1, num_people + 1):
-            solution[i - 1] = sum([(i + j*num_people) for j in range(num_rounds + 1 if i <= remaining else num_rounds)])
+            rounds_encountered = num_rounds + 1 if i <= remaining else num_rounds            
+            solution[i - 1] = int((rounds_encountered * i) + ((rounds_encountered - 1) * rounds_encountered / 2) * num_people)            
         solution[remaining] += int(candies - (pseudo_num_people * (pseudo_num_people + 1) / 2))
         return solution
